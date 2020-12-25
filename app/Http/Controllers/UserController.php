122cada0ef1d1637403b12894874273946c9d98a
Konstantin <glonas22808@mail.ru>
2020-12-25 13:58:17 +0300

    'Commit'

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,14 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function createUser(Request $request): object //Для работы с webhook
+
+
+    /**
+     * Данный метод обрабатывает данные с webhooks и с users.blade.php и отвечает за сохранение и изменение данных в бд
+     * @param Request $request
+     * @return object
+     */
+    public function createUser(Request $request): object
     {
         get8::updateOrCreate(['hash' => $request['hash']],
             ['name' => $request['name'],
@@ -20,6 +27,10 @@ class UserController extends Controller
             ]);
         return response()->json(['success' => 'OK']);
     }
+
+    /**
+     * Данный метод выводит все записи из бд используя пагинацию по 5 записей на страницу.
+     */
     public function showUser(){
      $user = get8::paginate(5);
      return view('users', compact('user'));
